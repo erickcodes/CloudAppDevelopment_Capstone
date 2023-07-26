b1763f34c9fdb445c8841436b738ba4eb74468e0
Erick Rivera <Erick.Rivera@gmail.com>
2023-07-26 15:55:25 -0400

    updated admin and models for the carmodel/make

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -4,24 +4,41 @@ from django.utils.timezone import now
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    # - Any other fields you would like to include in car make model
+
+    def __str__(self):
+        return "Make: " + self.name + ", Description: " + self.description
 
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
-# - Name
-# - Dealer id, used to refer a dealer created in cloudant database
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
-# - Year (DateField)
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake ,on_delete=models.CASCADE)
+    model = models.CharField(max_length=50)
+    dealer_id = models.IntegerField()
+    car_type = models.CharField(max_length=20)
+    year = models.DateField()
+       # - Any other fields you would like to include in car model
+
+    def __str__(self):
+        return "Model: " + self.model + ", Type: " + self.car_type 
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer(models.Model):
+    city = models.CharField(max_length=35)
+    state = models.CharField(max_length=20)
+    st = models.CharField(max_length=2)
+    address = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=5)
+    lat_cord = models.FloatField()
+    long_cord = models.FloatField()
+    short_name = models.CharField(max_length=25)
+    full_name = models.CharField(max_length=75)
 
+    def __str__(self):
+        return full_name + ", address: " + self.address + ", " + self.city + ", " + self.st
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
