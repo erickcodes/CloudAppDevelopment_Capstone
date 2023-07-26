240ad2c48d03699b3a46239b9b26b4fcb9b19ffa
Erick Rivera <Erick.Rivera@gmail.com>
2023-07-26 17:49:10 -0400

    Half way on dealer and review details

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -27,18 +27,53 @@ class CarModel(models.Model):
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
-class CarDealer(models.Model):
-    city = models.CharField(max_length=35)
-    state = models.CharField(max_length=20)
-    st = models.CharField(max_length=2)
-    address = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=5)
-    lat_cord = models.FloatField()
-    long_cord = models.FloatField()
-    short_name = models.CharField(max_length=25)
-    full_name = models.CharField(max_length=75)
+class CarDealer:
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state
+        self.st = st
+        # Dealer zip
+        self.zip = zip
 
     def __str__(self):
-        return full_name + ", address: " + self.address + ", " + self.city + ", " + self.st
+        return "Dealer name: " + self.full_name
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        # Buyer's dealership
+        self.dealership = dealership
+        # Buyer's name
+        self.name = name
+        # Buyer's purchase
+        self.purchase = purchase
+        # Review
+        self.review = review
+        # Buyer's purchase_date
+        self.purchase_date = purchase_date
+        # Buyer's car_make
+        self.car_make = car_make
+        # Buyer's car_model
+        self.car_model = car_model
+        # Buyer's car_year
+        self.car_year = car_year
+        # Buyer's sentiment
+        self.sentiment = sentiment
+        # Review ID
+        self.id = id
+
+    def __str__(self):
+        return "Reviewer's name: " + self.name
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -24,10 +24,12 @@ urlpatterns = [
     # path for logout    
     path(route='logout', view=views.logout_request, name='logout'),
 
+    # path for dealerships
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    
     # path for add a review view
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
